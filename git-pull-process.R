a888b62ffa1335cf796a17f884c99d7ba1691f75
zheng-ning <nyeon@yahoo.com>
2014-09-17 22:34:37 +0800

    test pull note

---
+++ git-pull-process.R
@@ -0,0 +1,8 @@
+$ mkdir testpull
+$ git mkdir init
+$ git remote add origin https://github.com/zheng-ning/testpull.git
+$ git pull ## it gives me an error massage
+$ git pull origin master
+$ git add .
+$ git commit -m "test to find pull must have origin master after it"
+$ git push
